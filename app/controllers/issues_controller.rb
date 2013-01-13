ed9836ff93d84252228cf5bcee3bf2daf9daa8d1
Nicklasos <nicklasos@gmail.com>
2013-01-13 17:52:22 +0200

    Add Complete model with association

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -4,43 +4,24 @@ class IssuesController < ApplicationController
 
   def index
     @issues = Issue.all
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @issues }
-    end
   end
 
   def show
     @issue = Issue.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @issue }
-    end
   end
 
   def new
     @issue = Issue.new
     @issue.questions.build
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @issue }
-    end
   end
 
   def create
     @issue = current_user.issues.new(params[:issue])
 
-    respond_to do |format|
-      if @issue.save
-        format.html { redirect_to @issue, notice: 'Issue was successfully created.' }
-        format.json { render json: @issue, status: :created, location: @issue }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @issue.errors, status: :unprocessable_entity }
-      end
+    if @issue.save
+      redirect_to @issue, notice: 'Issue was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
@@ -48,10 +29,7 @@ class IssuesController < ApplicationController
     @issue = current_user.issues.find(params[:id])
     @issue.destroy
 
-    respond_to do |format|
-      format.html { redirect_to issues_url }
-      format.json { head :no_content }
-    end
+    redirect_to issues_url
   end
 
   private
