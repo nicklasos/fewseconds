fb76b7dd7623ecbcc4186c88eb6836f9ffda54aa
Nicklasos <nicklasos@gmail.com>
2013-03-05 23:58:31 +0200

    Add new layout for login/registration pages

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,10 @@ module Fewseconds
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+
+    config.to_prepare do
+      Devise::SessionsController.layout proc{ |controller| action_name == 'new' ? "devise"   : "application" }
+    end
   end
 end
